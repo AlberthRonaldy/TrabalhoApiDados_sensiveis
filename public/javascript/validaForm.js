7ebed0068058820c0350a099585a809f84a6a445
Vinicius  Santana <111644554+ViniSantana179@users.noreply.github.com>
2024-05-07 00:02:21 -0300

    Adicionando Fetch para back no front

--- public/javascript/validaForm.js
+++ public/javascript/validaForm.js
@@ -30,6 +30,12 @@ function enviarFormulario() {
     confirmPassword: confirmPassword.value,
   };
 
+  for (const key in data) {
+    if (Object.hasOwnProperty.call(data, key)) {
+      data[key] = encryptData(data[key]);
+    }
+  }
+
   console.log(data);
 
   // Enviar os dados via POST para a API usando fetch
@@ -50,7 +56,30 @@ function enviarFormulario() {
     });
 }
 
+async function fetchPublicKey() {
+  try {
+    const response = await fetch("http://localhost:3000/public-key");
+    if (!response.ok) {
+      throw new Error("Erro ao buscar chave pública.");
+    }
+    const publicKey = await response.text();
+    return publicKey;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-function encode(value){
-    
-}   
+async function encryptData(data) {
+  try {
+    await generateKeys();
+    const publicKey = await fetchPublicKey();
+    if (!publicKey) return;
+
+    const encryptedData = await encrypt(data, publicKey);
+    console.log("Dados criptografados:", encryptedData);
+    // Enviar os dados criptografados para o back-end...
+  } catch (error) {
+    console.error(error);
+  }
+}
